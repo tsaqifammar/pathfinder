6cf177dcbd0c3c674330bb25cc259018425f6ed9
Muhammad Tsaqif Ammar <54428874+tsaqifammar@users.noreply.github.com>
2020-12-01 07:23:47 +0700

    Fixing a minor issue

--- js/script.js
+++ js/script.js
@@ -4,6 +4,6 @@ const r = 20, c = 40;
 // generate html grid
 var x = "";
 for (let i = 0; i < r*c; i++){
-	x += "<div class=\"cell\" id=\"cell-" + i + "\">_</div>";
+	x += "<div class=\"cell\" id=\"cell-" + i + "\"> </div>";
 }
 document.querySelector(".board").innerHTML = x;
